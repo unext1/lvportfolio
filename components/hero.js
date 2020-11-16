0bfdf283f74d734564f6309d16cb798b69e70824
Laurynas <laurys235@gmail.com>
2020-11-16 19:52:35 +0100

    skills fixed

--- components/hero.js
+++ components/hero.js
@@ -7,24 +7,26 @@ export default function Hero() {
             <div className="md:max-w-6xl md:mx-auto">
               <div className="md:flex md:flex-wrap">
                 <div className="text-center md:w-1/2 md:text-left md:pt-16">
-                  <h1 className="mb-4 text-3xl font-bold leading-tight text-white md:text-6xl">Laurynas Valiulis</h1>
+                  <h1 className="mb-4 text-3xl font-bold leading-tight text-white md:text-5xl lg:text-6xl ">
+                    Laurynas Valiulis
+                  </h1>
 
-                  <p className="text-indigo-200 md:text-lg md:pr-48">
+                  <p className="text-indigo-200 md:text-lg md:pr-32 lg:pr-48">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id vitae corrupti asperiores veritatis
                     dolorum, commodi aperiam enim.
                   </p>
 
                   <a
-                    href="/#journey"
+                    href="/#portfolio"
                     className="inline-block px-8 py-3 mt-6 mb-12 text-white duration-150 ease-in-out transform bg-blue-500 rounded-lg shadow md:mb-0 md:mt-10 hover:bg-blue-600"
                   >
-                    About Me
+                    My Portfolio
                   </a>
                 </div>
                 <div className="absolute right-0 md:w-1/2 ">
                   <div className="hidden md:block">
                     <div className="text-center">
-                      <i className="z-50 text-blue-500 fa fa-laptop" style={{ fontSize: '400px' }}></i>
+                      <i className="z-50 text-blue-500 fa fa-laptop loptop-size"></i>
                     </div>
                   </div>
                 </div>
--- components/skils.js
+++ components/skils.js
@@ -3,8 +3,8 @@ export default function Skils() {
     <>
       <div className="flex items-center text-gray-800" style={{ marginTop: '-8%' }} id="skils">
         <div className="w-full p-10">
-          <div className="grid h-24 grid-cols-12 gap-4">
-            <div className="col-span-12 shadow-md sm:col-span-6 md:col-span-3">
+          <div className="grid grid-cols-6 gap-4 md:grid-cols-8">
+            <div className="col-span-12 shadow-md sm:col-span-12 md:col-span-4 lg:col-span-2">
               <div className="flex flex-row h-full p-6 bg-gray-900 rounded shadow-sm">
                 <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-blue-500 bg-white rounded-full">
                   <i className="text-2xl fa fa-home"></i>
@@ -14,7 +14,7 @@ export default function Skils() {
                 </div>
               </div>
             </div>
-            <div className="col-span-12 shadow-md sm:col-span-6 md:col-span-3">
+            <div className="col-span-12 shadow-md sm:col-span-12 md:col-span-4 lg:col-span-2">
               <div className="flex flex-row p-6 bg-white rounded shadow-sm">
                 <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-gray-900 bg-gray-100 rounded-full">
                   <i className="text-2xl fa fa-home"></i>
@@ -24,7 +24,7 @@ export default function Skils() {
                 </div>
               </div>
             </div>
-            <div className="col-span-12 shadow-md sm:col-span-6 md:col-span-3">
+            <div className="col-span-12 shadow-md sm:col-span-12 md:col-span-4 lg:col-span-2">
               <div className="flex flex-row h-full p-6 bg-gray-900 rounded shadow-sm">
                 <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-blue-500 bg-white rounded-full">
                   <i className="text-2xl fa fa-home"></i>
@@ -34,7 +34,7 @@ export default function Skils() {
                 </div>
               </div>
             </div>
-            <div className="col-span-12 shadow-md sm:col-span-6 md:col-span-3">
+            <div className="col-span-12 shadow-md sm:col-span-12 md:col-span-4 lg:col-span-2">
               <div className="flex flex-row p-6 bg-white rounded shadow-sm">
                 <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-gray-900 bg-gray-100 rounded-full">
                   <i className="text-2xl fa fa-home"></i>
